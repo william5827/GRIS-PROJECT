76de53295e444fd95cb3145c6eb68ba274fc86db
william <william.matos@sptech.school>
2022-06-04 20:04:35 -0300

    GRIS-PROJECT 04.06 mural de posts feito

--- src/database/script_gris_project.sql
+++ src/database/script_gris_project.sql
@@ -10,3 +10,11 @@ CREATE TABLE usuario(
 	email VARCHAR(50) UNIQUE	,
 	senha VARCHAR(50)
 );
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
